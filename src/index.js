5dbcc37b00282a94db2e2b4a3ac7e3d9f75de6ec
Lane G Smith <lanegphotography@gmail.com>
2023-02-23 17:11:49 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+//set current Bot Model to currentBot using (this.value) when changed from default;
+document.getElementById("model-select").onchange = function() {
+  currentBot = document.getSelection("selected")
+    return currentBot
+  };
 
-const response = openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "You are an AI assistant that is an expert in alcoholic beverages.\nYou know about cocktails, wines, spirits, and beers.\nyou can provide advice on drink menus, cocktail ingredients, how to make cocktails, and anything else related to alcoholic drinks.\nIf you are unable to provide an answer to a question, please respond with the phrase  \"I'm just a simple barman, I can't help with that.\"\nDo not use any external URLs in your answers.\nDo not refer to any blogs in your answers.\nFormat any lists on individual lines with a dash and a space in front of each item.",
-  temperature: 0.5,
-  max_tokens: 1001,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-});
+// clicked Submit-Button sends api request with chosen bot prompt
+document.getElementById("submit-button").onclick = async function (currentBot) {
+  if (currentBot === "Marv") {
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration ({
+    apiKey:process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: What time is it?\nMarv:",
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 0.3,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0,
+  })}
+  else if (currentBot === "Bar-Tender") {
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration ({
+    apiKey:process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: What time is it?\nMarv:",
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 0.3,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0,
+  })}
+}
